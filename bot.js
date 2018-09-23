c14eca895a7244a55adf790ec32e79db12047531
fernandoMontes <fernando@element.in>
2018-09-23 22:19:49 +0200

    working basic solution

--- bot.js
+++ bot.js
@@ -1,56 +1,4 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
 
-
-This is a sample Slack bot built with Botkit.
-
-This bot demonstrates many of the core features of Botkit:
-
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
-
-# RUN THE BOT:
-
-  Create a new app via the Slack Developer site:
-
-    -> http://api.slack.com
-
-  Get a Botkit Studio token from Botkit.ai:
-
-    -> https://studio.botkit.ai/
-
-  Run your bot from the command line:
-
-    clientId=<MY SLACK TOKEN> clientSecret=<my client secret> PORT=<3000> studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js
-
-# USE THE BOT:
-
-    Navigate to the built-in login page:
-
-    https://<myhost.com>/login
-
-    This will authenticate you with Slack.
-
-    If successful, your bot will come online and greet you.
-
-
-# EXTEND THE BOT:
-
-  Botkit has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 var env = require('node-env-file');
 env(__dirname + '/.env');
 
@@ -66,10 +14,9 @@ var debug = require('debug')('botkit:main');
 var bot_options = {
     clientId: process.env.clientId,
     clientSecret: process.env.clientSecret,
-    // debug: true,
+    debug: true,
     scopes: ['bot'],
-    studio_token: process.env.studio_token,
-    studio_command_uri: process.env.studio_command_uri
+    token: process.env.API_TOKEN
 };
 
 // Use a mongo database if specified, otherwise store in a JSON file local to the app.
@@ -104,28 +51,19 @@ if (!process.env.clientId || !process.env.clientSecret) {
 
   // Set up a simple storage backend for keeping a record of customers
   // who sign up for the app via the oauth
-  require(__dirname + '/components/user_registration.js')(controller);
+  require(__dirname + '/components/rtm_manager.js')(controller);
 
-  // Send an onboarding message when a new team joins
-  require(__dirname + '/components/onboarding.js')(controller);
+
 
   var normalizedPath = require("path").join(__dirname, "skills");
   require("fs").readdirSync(normalizedPath).forEach(function (file) {
     require("./skills/" + file)(controller);
   });
 
-  // This captures and evaluates any message sent to the bot as a DM
-  // or sent to the bot in the form "@bot message" and passes it to
-  // Botkit Studio to evaluate for trigger words and patterns.
-  // If a trigger is matched, the conversation will automatically fire!
-  // You can tie into the execution of the script using the functions
-  // controller.studio.before, controller.studio.after and controller.studio.validate
-  controller.on('direct_message,direct_mention,mention', function (bot, message) {
-    console.log("received message");
-  });
+
+  controller.trigger('rtm:start', [bot_options]);
+
 }
-
-
 
 
 function usage_tip() {
---
+++ skills/sample.js
@@ -0,0 +1,10 @@
+
+module.exports = function(controller) {
+
+  controller.hears('hello','direct_mention,direct_message', function(bot, message) {
+
+    bot.reply(message,'Howdy!');
+
+  });
+
+}
