a7ef5351014c8182c1e9cdec368518724f10c09a
fernandoMontes <fernando@element.in>
2018-09-30 19:37:53 +0200

    - test webhook

--- src/components/routes/incoming_webhooks.ts
+++ src/components/routes/incoming_webhooks.ts
@@ -8,7 +8,7 @@ module.exports = (webserver, controller) => {
     // respond to Slack that the webhook has been received.
     res.status(200);
 
-    console.log(JSON.stringify(req));
+    console.log(req.toString());
     // Now, pass the webhook into be processed
     controller.handleWebhookPayload(req, res);
   });
