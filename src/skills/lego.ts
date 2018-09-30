c45551f6b98a176bb0f0713fbeb13b109b1ec182
fernandoMontes <fernando@element.in>
2018-09-30 20:32:22 +0200

    - test webhook

--- src/skills/lego.ts
+++ src/skills/lego.ts
@@ -147,6 +147,8 @@ function legoMessage() : SlackMessage {
 
 module.exports = controller => {
   controller.hears('lego', 'direct_mention,direct_message', (bot, message) => {
+    console.log(JSON.stringify(message))
+    console.log(JSON.stringify(bot))
     controller.storage.teams.save({id: message.team, foo:'bar', bot:{}}, function(err) { console.log(err) });
     bot.reply(message, legoMessage());
   });
