c9d7be22d653d97826a76e08a72cc85cf24251e5
fernandoMontes <fernando@element.in>
2018-10-03 16:30:29 +0200

    - test reply

--- src/skills/lego.ts
+++ src/skills/lego.ts
@@ -18,28 +18,28 @@ function legoMessage() : SlackMessage {
             "options": [
               {
                 "text": "1",
-                "value": "1"
+                "value": 1
               }, {
                 "text": "2",
-                "value": "2"
+                "value": 2
               }, {
                 "text": "3",
-                "value": "3"
+                "value": 3
               }, {
                 "text": "4",
-                "value": "4"
+                "value": 4
               }, {
                 "text": "5",
-                "value": "5"
+                "value": 5
               }, {
                 "text": "6",
-                "value": "6"
+                "value": 6
               }, {
                 "text": "7",
-                "value": "7"
+                "value": 7
               }, {
                 "text": "8",
-                "value": "8"
+                "value": 8
               }
             ]
           },
@@ -50,28 +50,28 @@ function legoMessage() : SlackMessage {
             "options": [
               {
                 "text": "1",
-                "value": "1"
+                "value": 1
               }, {
                 "text": "2",
-                "value": "2"
+                "value": 2
               }, {
                 "text": "3",
-                "value": "3"
+                "value": 3
               }, {
                 "text": "4",
-                "value": "4"
+                "value": 4
               }, {
                 "text": "5",
-                "value": "5"
+                "value": 5
               }, {
                 "text": "6",
-                "value": "6"
+                "value": 6
               }, {
                 "text": "7",
-                "value": "7"
+                "value": 7
               }, {
                 "text": "8",
-                "value": "8"
+                "value": 8
               }
             ]
           },
@@ -82,28 +82,28 @@ function legoMessage() : SlackMessage {
             "options": [
               {
                 "text": "1",
-                "value": "1"
+                "value": 1
               }, {
                 "text": "2",
-                "value": "2"
+                "value": 2
               }, {
                 "text": "3",
-                "value": "3"
+                "value": 3
               }, {
                 "text": "4",
-                "value": "4"
+                "value": 4
               }, {
                 "text": "5",
-                "value": "5"
+                "value": 5
               }, {
                 "text": "6",
-                "value": "6"
+                "value": 6
               }, {
                 "text": "7",
-                "value": "7"
+                "value": 7
               }, {
                 "text": "8",
-                "value": "8"
+                "value": 8
               }
             ]
           },
@@ -114,28 +114,28 @@ function legoMessage() : SlackMessage {
             "options": [
               {
                 "text": "1",
-                "value": "1"
+                "value": 1
               }, {
                 "text": "2",
-                "value": "2"
+                "value": 2
               }, {
                 "text": "3",
-                "value": "3"
+                "value": 3
               }, {
                 "text": "4",
-                "value": "4"
+                "value": 4
               }, {
                 "text": "5",
-                "value": "5"
+                "value": 5
               }, {
                 "text": "6",
-                "value": "6"
+                "value": 6
               }, {
                 "text": "7",
-                "value": "7"
+                "value": 7
               }, {
                 "text": "8",
-                "value": "8"
+                "value": 8
               }
             ]
           }
