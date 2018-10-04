076f7c98434599d23fd3ced10e4aaba928b31a40
fernandoMontes <fernando@element.in>
2018-10-04 22:30:41 +0200

    - cleanup - Only the last message is visible after selecting

--- src/skills/interactive_messages.ts
+++ src/skills/interactive_messages.ts
@@ -1,40 +1,55 @@
-import { SlackAttachment, SlackBot } from 'botkit';
+import { SlackAttachment, SlackBot, SlackMessage } from 'botkit';
+
+let oldMessages: any = {};
+
+function createSummaryMessage(message): SlackAttachment {
+  let oldMessage = oldMessages[message.original_message.ts] || {};
+  if (!oldMessage) {
+    oldMessages[message.original_message.ts] = oldMessage;
+  }
+
+  let messageStr: String = '';
+  for (let currentAction of message.actions) {
+    let statusForAction = oldMessage[currentAction.name] || {};
+    let entryForUser = statusForAction[message.user] || {
+      value: currentAction.selected_options[0].value,
+    };
+
+    statusForAction[message.user] = entryForUser;
+    oldMessage[currentAction.name] = statusForAction;
+
+    console.log(`oldMessages: ${JSON.stringify(oldMessages)}`);
+
+    let person = `<@${message.user}>`;
+
+    return {
+      text: `${person} said, ${messageStr}`,
+    };
+  }
+  return {};
+}
+
+function createResponse(message): SlackMessage {
+  const reply: SlackMessage = message.original_message;
+
+  let attachmentsToSend: SlackAttachment[] = reply.attachments.filter(
+    attachment => attachment.callback_id === 'lego_stats'
+  );
+
+  let replyStatusMessage: SlackAttachment = createSummaryMessage(message);
+  attachmentsToSend.push(replyStatusMessage);
+
+  reply.attachments = attachmentsToSend;
+  return reply;
+}
 
 module.exports = controller => {
   controller.middleware.receive.use((bot: SlackBot, message, next) => {
     if (message.type == 'interactive_message_callback' && message.actions) {
       if (message.actions[0].name.match(/^lego-select-option-/)) {
-        const reply = message.original_message;
+        console.log(`tes: ${message.original_message.ts}`);
 
-        let attachmentsToSend: SlackAttachment[] = reply.attachments.filter(
-          attachment => attachment.callback_id === 'lego_stats'
-        );
-
-        // for (let a = 0; a < reply.attachments.length; a++) {
-          //   reply.attachments[a].actions = null;
-          // }
-
-        let messageStr = '';
-        for (let currentAction of message.actions) {
-          for (let selectOption of currentAction.selected_options) {
-            messageStr += `${currentAction.name} : ${selectOption.value}`;
-          }
-        }
-
-        console.log(`message: ${messageStr}`);
-
-        let person = '<@' + message.user + '>';
-        if (message.channel[0] == 'D') {
-          person = 'You';
-        }
-
-        attachmentsToSend.push({
-          text: `${person} said, ${messageStr}`,
-        });
-
-        reply.attachments = attachmentsToSend;
-
-        bot.replyInteractive(message, reply);
+        bot.replyInteractive(message, createResponse(message));
       }
     }
 
