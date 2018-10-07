7be79a8eef0c00a315167bcc78ec6ad2de6bf334
fernandoMontes <fernando@element.in>
2018-10-07 21:12:35 +0200

    - Added help command

--- src/skills/channel_join.ts
+++ src/skills/channel_join.ts
@@ -2,7 +2,7 @@ import { SlackBot, SlackController, SlackMessage } from 'botkit';
 
 const OnChannelJoinHandler = (controller: SlackController): void => {
   controller.on('bot_channel_join', (bot: SlackBot, message: SlackMessage) => {
-    const helloMessage:string =
+    const helloMessage: string =
       'Hello, Thanks for inviting me! I will be helping you to gather some metrics for the team. Try me just mention help';
     bot.reply(message, helloMessage);
   });
---
+++ src/skills/help.ts
@@ -0,0 +1,25 @@
+import { SlackBot, SlackController, SlackMessage } from 'botkit';
+
+const summary: any = [
+  {
+    command: 'lego',
+    description: 'Helps channel tracking the time spent on tasks.',
+  },
+  { command: 'help', description: 'Prints this menu.' },
+  { command: 'uptime', description: 'Internal bot statistics.' },
+];
+
+const helpHandler = (controller: SlackController): void => {
+  controller.hears(
+    'help',
+    'direct_mention',
+    (bot: SlackBot, message: SlackMessage) => {
+      let messageStr = summary
+        .map(item => `${item.command} - ${item.description}`)
+        .join('\n');
+
+      bot.reply(message, messageStr);
+    }
+  );
+};
+module.exports = helpHandler;
