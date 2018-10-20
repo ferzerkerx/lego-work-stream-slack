cb72f78be967b1bb7fe8e1188ee4e9984269c7d9
fernandoMontes <fernando@element.in>
2018-10-20 19:40:52 +0200

    - cleanup

--- src/public/js/metrics.js
+++ src/public/js/metrics.js
@@ -1,179 +1,184 @@
-const svg = d3.select('#stacked'),
-  tooltip = d3.select('#tooltip'),
-  margin = { top: 10, right: 10, bottom: 20, left: 40 },
-  width = +svg.attr('width') - margin.left - margin.right,
-  height = +svg.attr('height') - margin.top - margin.bottom;
+const metricsComponent = (() => {
+  const svg = d3.select('#stacked'),
+    tooltip = d3.select('#tooltip'),
+    margin = { top: 10, right: 10, bottom: 20, left: 40 },
+    width = +svg.attr('width') - margin.left - margin.right,
+    height = +svg.attr('height') - margin.top - margin.bottom;
 
-function addDays(dateString) {
-  const referenceDate = new Date(dateString);
-  return new Date(new Date().setDate(referenceDate.getDate() + 1));
-}
-
-function toNumber(value) {
-  const numberValue = Number(value);
-  if (isNaN(numberValue)) {
-    return null;
+  function addDays(dateString) {
+    const referenceDate = new Date(dateString);
+    return new Date(new Date().setDate(referenceDate.getDate() + 1));
   }
-  return numberValue;
-}
 
-function toString(theDate) {
-  return theDate.toJSON().slice(0, 10);
-}
-
-function createConfiguration() {
-  const form = document.forms[0];
-  const startDate = form.startDate.value || toString(new Date());
-  const endDate = form.endDate.value || toString(addDays(startDate, 1));
-  const frequency = toNumber(form.frequency.value) || 1;
-  const isPercentage = form.isPercentage.checked || false;
-
-  return {
-    startDate: startDate,
-    endDate: endDate,
-    frequency: frequency,
-    isPercentage: isPercentage,
-  };
-}
-
-function url(config) {
-  return `/api/metrics?startDate=${config.startDate}&endDate=${
-    config.endDate
-  }&frequency=${config.frequency}&isPercentage=${config.isPercentage}`;
-}
-
-function createDataFromResponse(jsonResponse) {
-  const { categories, entries } = jsonResponse;
-
-  const datesToDisplay = entries.map(entry => entry.date);
-
-  const totalCountsPerDates = entries.map(entry => {
-    return d3.sum(categories.map(key => entry.valuesByCategory[key]));
-  });
-
-  const orderOfDates = d3.range(entries.length);
-
-  const sumsPerDateAndCategory = entries.map(entry =>
-    Array.from(categories, category => entry.valuesByCategory[category] || 0)
-  );
-
-  return Object.assign(
-    d3.stack().keys(d3.range(categories.length))(
-      d3.permute(sumsPerDateAndCategory, orderOfDates)
-    ),
-    {
-      categories: Array.from(categories),
-      totals: d3.permute(totalCountsPerDates, orderOfDates),
-      names: d3.permute(datesToDisplay, orderOfDates),
+  function toNumber(value) {
+    const numberValue = Number(value);
+    if (isNaN(numberValue)) {
+      return null;
     }
-  );
-}
+    return numberValue;
+  }
 
-function renderMetrics(evt) {
-  evt.preventDefault();
-  svg.selectAll('*').remove();
+  function toString(theDate) {
+    return theDate.toJSON().slice(0, 10);
+  }
 
-  const config = createConfiguration();
+  function createConfiguration() {
+    const form = document.forms[0];
+    const startDate = form.startDate.value || toString(new Date());
+    const endDate = form.endDate.value || toString(addDays(startDate, 1));
+    const frequency = toNumber(form.frequency.value) || 1;
+    const isPercentage = form.isPercentage.checked || false;
 
-  const url = url(config);
-  d3.json(url).then(jsonResponse => {
-    const data = createDataFromResponse(jsonResponse);
+    return {
+      startDate: startDate,
+      endDate: endDate,
+      frequency: frequency,
+      isPercentage: isPercentage,
+    };
+  }
 
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data.totals)])
-      .rangeRound([height - margin.bottom, margin.top]);
+  function createUrl(config) {
+    return `/api/metrics?startDate=${config.startDate}&endDate=${
+      config.endDate
+    }&frequency=${config.frequency}&isPercentage=${config.isPercentage}`;
+  }
 
-    const x = d3
-      .scaleBand()
-      .domain(data.names)
-      .range([margin.left, width - margin.right])
-      .padding(0.1);
+  function createDataFromResponse(jsonResponse) {
+    const { categories, entries } = jsonResponse;
 
-    const yAxis = g =>
-      g
-        .attr('transform', `translate(${margin.left},0)`)
-        .call(d3.axisLeft(y).ticks(null, 's'))
-        .call(g => g.selectAll('.domain').remove());
+    const datesToDisplay = entries.map(entry => entry.date);
 
-    const xAxis = g =>
-      g
-        .attr('transform', `translate(0,${height - margin.bottom})`)
-        .call(d3.axisBottom(x).tickSizeOuter(0))
-        .call(g => g.selectAll('.domain').remove());
+    const totalCountsPerDates = entries.map(entry => {
+      return d3.sum(categories.map(key => entry.valuesByCategory[key]));
+    });
 
-    svg
-      .append('g')
-      .selectAll('g')
-      .data(data)
-      .enter()
-      .append('g')
-      .attr('fill', (d, i) => data.categories[i])
-      .on('mouseover', (d, i) => {
-        const displayText = `${Math.abs(d[0][0] - d[0][1])} - ${
-          data.categories[i]
-        } ${config.isPercentage ? '%' : ''}`;
+    const orderOfDates = d3.range(entries.length);
 
-        tooltip.style('opacity', 0.9);
+    const sumsPerDateAndCategory = entries.map(entry =>
+      Array.from(categories, category => entry.valuesByCategory[category] || 0)
+    );
 
-        tooltip
-          .html(`${displayText}`)
-          .attr('class', `d3-tip`)
-          .style('left', `${d3.event.pageX + 30}px`)
-          .style('top', `${d3.event.pageY - 30}px`);
-      })
-      .on('mouseout', () => {
-        tooltip.style('opacity', 0);
-      })
-      .selectAll('rect')
-      .data(d => d)
-      .enter()
-      .append('rect')
-      .attr('x', (d, i) => x(data.names[i]))
-      .attr('y', d => y(d[1]))
-      .attr('height', d => y(d[0]) - y(d[1]))
-      .attr('width', x.bandwidth());
+    return Object.assign(
+      d3.stack().keys(d3.range(categories.length))(
+        d3.permute(sumsPerDateAndCategory, orderOfDates)
+      ),
+      {
+        categories: Array.from(categories),
+        totals: d3.permute(totalCountsPerDates, orderOfDates),
+        names: d3.permute(datesToDisplay, orderOfDates),
+      }
+    );
+  }
 
-    svg.append('g').call(xAxis);
+  function render(evt) {
+    evt.preventDefault();
+    svg.selectAll('*').remove();
 
-    svg.append('g').call(yAxis);
+    const config = createConfiguration();
 
-    svg
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 0 - 10)
-      .attr('x', 0 - height / 2)
-      .attr('dy', '1em')
-      .style('text-anchor', 'middle')
-      .text(`${config.isPercentage ? 'Percentages' : 'Values'}`);
+    const url = createUrl(config);
+    d3.json(url).then(jsonResponse => {
+      const data = createDataFromResponse(jsonResponse);
 
-    const legend = svg => {
-      const g = svg
-        .attr('font-family', 'sans-serif')
-        .attr('font-size', 10)
-        .attr('text-anchor', 'end')
+      const y = d3
+        .scaleLinear()
+        .domain([0, d3.max(data.totals)])
+        .rangeRound([height - margin.bottom, margin.top]);
+
+      const x = d3
+        .scaleBand()
+        .domain(data.names)
+        .range([margin.left, width - margin.right])
+        .padding(0.1);
+
+      const yAxis = g =>
+        g
+          .attr('transform', `translate(${margin.left},0)`)
+          .call(d3.axisLeft(y).ticks(null, 's'))
+          .call(g => g.selectAll('.domain').remove());
+
+      const xAxis = g =>
+        g
+          .attr('transform', `translate(0,${height - margin.bottom})`)
+          .call(d3.axisBottom(x).tickSizeOuter(0))
+          .call(g => g.selectAll('.domain').remove());
+
+      svg
+        .append('g')
         .selectAll('g')
-        .data(data.categories.slice())
+        .data(data)
         .enter()
         .append('g')
-        .attr('transform', (d, i) => `translate(0,${i * 20})`);
+        .attr('fill', (d, i) => data.categories[i])
+        .on('mouseover', (d, i) => {
+          const displayText = `${Math.abs(d[0][0] - d[0][1])} - ${
+            data.categories[i]
+          } ${config.isPercentage ? '%' : ''}`;
 
-      g.append('rect')
-        .attr('x', 29)
-        .attr('width', 19)
-        .attr('height', 19)
-        .attr('fill', (d, i) => data.categories[i]);
+          tooltip.style('opacity', 0.9);
 
-      g.append('text')
-        .attr('x', 24)
-        .attr('y', 9.5)
-        .attr('dy', '0.35em')
-        .text(d => d);
-    };
+          tooltip
+            .html(`${displayText}`)
+            .attr('class', `d3-tip`)
+            .style('left', `${d3.event.pageX + 30}px`)
+            .style('top', `${d3.event.pageY - 30}px`);
+        })
+        .on('mouseout', () => {
+          tooltip.style('opacity', 0);
+        })
+        .selectAll('rect')
+        .data(d => d)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => x(data.names[i]))
+        .attr('y', d => y(d[1]))
+        .attr('height', d => y(d[0]) - y(d[1]))
+        .attr('width', x.bandwidth());
 
-    svg
-      .append('g')
-      .attr('transform', `translate(${width - margin.right},${margin.top})`)
-      .call(legend);
-  });
-}
+      svg.append('g').call(xAxis);
+
+      svg.append('g').call(yAxis);
+
+      svg
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .attr('y', 0 - 2)
+        .attr('x', 0 - height / 2)
+        .attr('dy', '1em')
+        .style('text-anchor', 'middle')
+        .text(`${config.isPercentage ? 'Percentages' : 'Values'}`);
+
+      const legend = svg => {
+        const g = svg
+          .attr('font-family', 'sans-serif')
+          .attr('font-size', 10)
+          .attr('text-anchor', 'end')
+          .selectAll('g')
+          .data(data.categories.slice())
+          .enter()
+          .append('g')
+          .attr('transform', (d, i) => `translate(0,${i * 20})`);
+
+        g.append('rect')
+          .attr('x', 29)
+          .attr('width', 19)
+          .attr('height', 19)
+          .attr('fill', (d, i) => data.categories[i]);
+
+        g.append('text')
+          .attr('x', 24)
+          .attr('y', 9.5)
+          .attr('dy', '0.35em')
+          .text(d => d);
+      };
+
+      svg
+        .append('g')
+        .attr('transform', `translate(${width - margin.right},${margin.top})`)
+        .call(legend);
+    });
+  }
+  return {
+    render: render,
+  };
+})();
