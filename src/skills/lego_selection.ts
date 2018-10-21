67b72917c745c2be4fb968e6719531ea0c4f144e
fernandoMontes <fernando@element.in>
2018-10-21 12:22:44 +0200

    - added some styling - updated bootstrap - refactored a bit

--- src/skills/lego_selection.ts
+++ src/skills/lego_selection.ts
@@ -5,25 +5,29 @@ import {
 } from '../lego/LegoMessageFactory';
 
 const legoSelectionHandler = (controller: SlackController): void => {
+  function defaultConfiguration(): LegoMessageConfig {
+    return {
+      //TODO  retrieve this config from db
+      actionDescriptors: [
+        { name: 'green', text: 'Green' },
+        { name: 'red', text: 'Red' },
+        { name: 'orange', text: 'Orange' },
+        { name: 'black', text: 'Black' },
+      ],
+      min: 0,
+      max: 8,
+    };
+  }
+
   controller.hears(
     'lego',
     'direct_mention',
     (bot: SlackBot, message: SlackMessage) => {
-      let defaultConfig: LegoMessageConfig = {
-        //TODO  retrieve this config from db
-        actionDescriptors: [
-          { name: 'green', text: 'Green' },
-          { name: 'red', text: 'Red' },
-          { name: 'orange', text: 'Orange' },
-          { name: 'black', text: 'Black' },
-        ],
-        min: 0,
-        max: 8,
-      };
+      let configuration: LegoMessageConfig = defaultConfiguration();
 
       bot.reply(
         message,
-        LegoMessageFactory.createMessage(defaultConfig, new Date())
+        LegoMessageFactory.createMessage(configuration, new Date())
       );
     }
   );
