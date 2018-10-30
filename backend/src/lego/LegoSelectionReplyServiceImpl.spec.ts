fb673674ad3db28d43c4972f016c50fad6ab691b
fernandoMontes <fernando@element.in>
2018-10-30 22:21:58 +0100

    - added test

--- backend/src/lego/LegoSelectionReplyServiceImpl.spec.ts
+++ backend/src/lego/LegoSelectionReplyServiceImpl.spec.ts
@@ -1,5 +1,7 @@
 import { LegoSelectedValue } from './LegoSelectedValue';
 import { LegoSelectionReplyServiceImpl } from './LegoSelectionReplyServiceImpl';
+import { LegoSelectMessageRepository } from './Types';
+import { LegoSelectMessage } from './LegoSelectMessage';
 
 describe('LegoSelectionReplyService', () => {
   test('formatMessage properly', () => {
@@ -44,4 +46,58 @@ describe('LegoSelectionReplyService', () => {
     );
     expect(legoSelectMessage).toMatchSnapshot();
   });
+
+  test('createReply', () => {
+    const legoSelectionReplyServiceImpl = new LegoSelectionReplyServiceImpl(
+      new FakeRepo()
+    );
+    const message = {
+      ts: 12345,
+      channel: 'channelName',
+      original_message: {
+        ts: 12345,
+        attachments: [
+          {
+            callback_id: 'lego_stats',
+            actions: [{ name: 'someActionName', text: 'someActionText' }],
+          },
+        ],
+      },
+      user: 'someUserId',
+      raw_message: {
+        user: { name: 'someuser' },
+        channel: { name: 'channelName' },
+      },
+      actions: [{ name: 'someActionName', selected_options: [{ value: 8 }] }],
+    };
+
+    legoSelectionReplyServiceImpl.createReply(message).then(slackMessage => {
+      expect(slackMessage).toMatchSnapshot();
+    });
+  });
+
+  class FakeRepo implements LegoSelectMessageRepository {
+    find(messageId: string): Promise<LegoSelectMessage> {
+      const legoSelectMessage = new LegoSelectMessage();
+      legoSelectMessage.selectedValues.push({
+        id: 'someActionName',
+        entries: [
+          {
+            userData: { userId: 'someUserId', userName: 'someuser' },
+            value: 5,
+          },
+        ],
+      });
+
+      return Promise.resolve(legoSelectMessage);
+    }
+
+    findMessagesBy(config: MetricsConfiguration): Promise<LegoSelectMessage[]> {
+      return Promise.resolve([]);
+    }
+
+    save(legoMessage: LegoSelectMessage): Promise<LegoSelectMessage> {
+      return Promise.resolve(legoMessage);
+    }
+  }
 });
