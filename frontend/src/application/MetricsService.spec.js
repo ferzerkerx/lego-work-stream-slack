92c67da787701b6da16fc794e660253955379ff3
fernandoMontes <fernando@element.in>
2018-10-30 23:51:09 +0100

    - added test

---
+++ frontend/src/application/MetricsService.spec.js
@@ -0,0 +1,27 @@
+import { MetricsService } from './MetricsService';
+
+const fakeForm = () => {
+  return {
+    startDate: { value: '2018-10-01' },
+    endDate: { value: '2018-10-01' },
+    frequency: { value: 10 },
+    isPercentage: { checked: true },
+    teams: { value: 'team1,team2' },
+  };
+};
+
+describe('MetricsService', () => {
+
+  test('createConfiguration', () => {
+    const configuration = MetricsService.createConfiguration(fakeForm());
+    expect(configuration).toMatchSnapshot();
+  });
+
+  test('createUrl', () => {
+    const configuration = MetricsService.createConfiguration(fakeForm());
+    configuration.format = 'json'
+    const url = MetricsService.createUrl(configuration);
+    expect(url).toMatchSnapshot();
+  });
+
+});
