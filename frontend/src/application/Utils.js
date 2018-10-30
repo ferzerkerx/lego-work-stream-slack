8c8127660767e1a6580fc8bc295d7f3280cdaac4
fernandoMontes <fernando@element.in>
2018-10-30 23:31:51 +0100

    - added test

--- frontend/src/application/Utils.js
+++ frontend/src/application/Utils.js
@@ -2,10 +2,10 @@ class Utils {
   static addDays(dateString) {
     const referenceDate = new Date(dateString);
     const date = new Date(new Date().setDate(referenceDate.getDate() + 1));
-    date.setSeconds(0);
-    date.setMilliseconds(0);
-    date.setHours(0);
-    date.setMinutes(0);
+    date.setUTCHours(0);
+    date.setUTCMinutes(0);
+    date.setUTCSeconds(0);
+    date.setUTCMilliseconds(0);
     return date;
   }
 
